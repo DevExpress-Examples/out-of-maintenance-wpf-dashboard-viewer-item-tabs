f9fb547f58fc2757ec9805fe154bfc11b7a745db
DevExpressExampleBot <fakeEmail>
2018-12-10 16:31:08 +0300

    Source auto update [skip ci]

---
+++ VB/WpfDashboardTabSupportExample/WpfDashboardTabSupportExample/DashboardItemGenerator.vb
@@ -0,0 +1,63 @@
+﻿Imports DevExpress.DashboardCommon
+
+Namespace WpfDashboardTabSupportExample
+    Public NotInheritable Class DashboardItemGenerator
+
+        Private Sub New()
+        End Sub
+
+        Public Shared Function GenerateCardItem(ByVal dataSource As IDashboardDataSource, ByVal itemComponentName As String) As DashboardItem
+            Dim cardItem As New CardDashboardItem() With {.ComponentName= itemComponentName}
+            cardItem.SeriesDimensions.Add(New Dimension("Country"))
+            cardItem.SparklineArgument = New Dimension("SalesDate", DateTimeGroupInterval.DayMonthYear)
+            cardItem.DataSource = dataSource
+            cardItem.ShowCaption = False
+
+            Dim actualValue As New Measure("Sales")
+            actualValue.NumericFormat.FormatType = DataItemNumericFormatType.Currency
+            Dim targetValue As New Measure("SalesTarget")
+            targetValue.NumericFormat.FormatType = DataItemNumericFormatType.Currency
+            Dim card As New Card(actualValue, targetValue)
+            card.LayoutTemplate = New CardCompactLayoutTemplate()
+            cardItem.Cards.Add(card)
+
+            Return cardItem
+        End Function
+
+        Public Shared Function GenerateListBoxItem(ByVal dataSource As IDashboardDataSource, ByVal itemComponentName As String) As DashboardItem
+            Dim listBoxItem As New ListBoxDashboardItem() With {.ComponentName = itemComponentName}
+            listBoxItem.Name = "Countries"
+            listBoxItem.DataSource = dataSource
+            listBoxItem.FilterDimensions.Add(New Dimension("Country"))
+            listBoxItem.InteractivityOptions.IgnoreMasterFilters = False
+
+            Return listBoxItem
+        End Function
+
+        Public Shared Function GeneratePieItem(ByVal dataSource As IDashboardDataSource, ByVal itemComponentName As String) As DashboardItem
+            Dim pieItem As New PieDashboardItem() With {.ComponentName = itemComponentName}
+            pieItem.DataSource = dataSource
+            pieItem.Values.Add(New Measure("Sales"))
+            pieItem.Arguments.Add(New Dimension("Country"))
+            pieItem.SeriesDimensions.Add(New Dimension("SalesDate"))
+
+            Return pieItem
+        End Function
+
+        Public Shared Function GenerateGridItem(ByVal dataSource As IDashboardDataSource, ByVal itemComponentName As String) As DashboardItem
+            Dim gridItem As New GridDashboardItem() With {.ComponentName = itemComponentName}
+            gridItem.DataSource = dataSource
+
+            gridItem.Columns.Add(New GridDimensionColumn(New Dimension("Country")))
+            gridItem.Columns.Add(New GridMeasureColumn(New Measure("Sales")))
+            gridItem.Columns.Add(New GridDeltaColumn(New Measure("Sales"), New Measure("SalesTarget")))
+            gridItem.Columns.Add(New GridSparklineColumn(New Measure("Sales")))
+            gridItem.SparklineArgument = New Dimension("SalesDate", DateTimeGroupInterval.MonthYear)
+
+            Return gridItem
+        End Function
+
+
+
+    End Class
+End Namespace
---
+++ VB/WpfDashboardTabSupportExample/WpfDashboardTabSupportExample/DataGenerator.vb
@@ -0,0 +1,51 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.IO
+
+Namespace WpfDashboardTabSupportExample
+    Public NotInheritable Class DataGenerator
+
+        Private Sub New()
+        End Sub
+
+        Public Shared Function GenerateTestData() As List(Of DataRow)
+            Dim data As New List(Of DataRow)()
+            Dim rand As New Random(Date.Now.Second)
+
+            Dim countries = New String() { "USA", "Canada", "Argentina", "Brazil" }
+            For Each country As String In countries
+                For i As Integer = 0 To 99
+                    data.Add(New DataRow With { _
+                        .Country = country, _
+                        .Sales = rand.NextDouble() * 100 * i, _
+                        .SalesTarget = rand.NextDouble() * 100 * i, _
+                        .SalesDate = Date.Now.AddDays(((If(i Mod 2 = 0, -1, 1)) * i) + rand.Next(10, 40)) _
+                    })
+                Next i
+            Next country
+            Return data
+        End Function
+
+        Public Shared Sub SaveTestData()
+            Dim data As List(Of DataRow) = GenerateTestData()
+            Using stream = New StreamWriter("data.csv") With {.NewLine = vbLf}
+                stream.WriteLine("Country,Sales,SalesTarget,SalesDate")
+                For Each datarow In data
+                    stream.WriteLine(datarow.ToCsv())
+                Next datarow
+            End Using
+        End Sub
+
+    End Class
+
+    Public Class DataRow
+        Public Property Country() As String
+        Public Property Sales() As Double
+        Public Property SalesTarget() As Double
+        Public Property SalesDate() As Date
+
+        Public Function ToCsv() As String
+            Return String.Join(",", Me.Country, Me.Sales.ToString(), Me.SalesTarget.ToString(), Me.SalesDate.ToString())
+        End Function
+    End Class
+End Namespace
